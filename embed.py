6853827c9a5b88eab9585b440f008e6a4870c337
ramv <ram.venkat.11@gmail.com>
2026-01-07 17:52:34 -0500

    Vector representing the entire passage messed up memory usage. Fixed.

--- embed.py
+++ embed.py
@@ -1,31 +1,13 @@
 import os
-import time
 from typing import Any
-
 from fastembed import TextEmbedding
-from fastembed.common.model_description import PoolingType, ModelSource
+
 
 class Embed:
     embedder: TextEmbedding | None = None
 
     @classmethod
     def init_embedder(cls) -> None:
-        # print(TextEmbedding.list_supported_models())
-        # if cls.embedder is None:
-        #     TextEmbedding.add_custom_model(
-        #         model="intfloat/multilingual-e5-large3",
-        #         pooling=PoolingType.MEAN,
-        #         normalization=True,
-        #         dim=1024,
-        #         license="mit",
-        #         model_file="model.onnx",
-        #         additional_files=["model.onnx_data"],
-        #         sources=ModelSource(
-        #             hf="qdrant/multilingual-e5-large-onnx",
-        #             url="https://storage.googleapis.com/qdrant-fastembed/fast-multilingual-e5-large.tar.gz",
-        #             _deprecated_tar_struct=True,
-        #         )
-        #     )
         model_dir = None
         for d in ("/opt/models/fast-multilingual-e5-large", "./models/fast-multilingual-e5-large"):
             if os.path.exists(d):
@@ -34,18 +16,36 @@ class Embed:
         if model_dir is not None:
             cls.embedder = TextEmbedding(model_name="intfloat/multilingual-e5-large", specific_model_path=model_dir)
 
-
     @classmethod
     def get_vectors(cls, texts: list[str]) -> list[list[float]]:
-        if cls.embedder is not None:
-            embeddings = list(cls.embedder.embed(texts))
-            return [e.tolist() for e in embeddings]
-        else:
+        if cls.embedder is None:
             return []
+        out: list[list[float]] = []
+        i = 0
+        for e in cls.embedder.embed(texts[:-1]):
+            out.append(e.tolist())
+            # print(f"{i + 1}: text: {texts[i]}, Memory: {_rss_bytes_linux_procfs()}")
+            i += 1
+        print(f"Memory after questions: {_rss_bytes_linux_procfs()}")
+        for e in cls.embedder.embed(texts[-1:]):
+            out.append(e.tolist())
+        # print(f"text: Whole passage, Memory: {_rss_bytes_linux_procfs()}")
+        return out
+
+
+def _rss_bytes_linux_procfs() -> str:
+    with open("/proc/self/statm", "r", encoding="utf-8") as f:
+        resident_pages = int(f.read().split()[1])
+    page_size = os.sysconf("SC_PAGE_SIZE")
+    b = resident_pages * page_size
+    return f"{b / (1024 * 1024):,.1f} MB"
 
 Embed.init_embedder()
 
-
 def lambda_handler(event: dict[str, Any], context) -> list[list[float]]:
-    return Embed.get_vectors(event.get("texts", []))
+    texts = event.get("texts", [])
+    print(f"Number of texts: {len(texts)}, Memory before: {_rss_bytes_linux_procfs()}")
+    vectors = Embed.get_vectors(texts)
+    print(f"Memory before returning: {_rss_bytes_linux_procfs()}")
+    return vectors
 
